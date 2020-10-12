70dc8c400a9a74187121ba3758e24a8940c2287f
CutieMath <cutie.math@protonmail.com>
2020-10-12 16:12:12 +1100

    variable hoisting

--- 2-howItWorks/script.js
+++ 2-howItWorks/script.js
@@ -3,22 +3,41 @@
  ************/
 
 
-// 1. Functions
-calculate(1997);
+// // 1. Functions
+// calculate(1997);
 
-// a. Function Declaration 
-// the function is stored before execution
-function calculate(year){
-    console.log(2020 - year);
+// // a. Function Declaration 
+// // the function is stored before execution
+// function calculate(year){
+//     console.log(2020 - year);
+// }
+
+
+// // b. Function Expression
+// // the function can only be called after declaration 
+// var retire = function(year){
+//     console.log(60 - (2020 - year));
+// }
+// retire(1997);
+
+
+// 2. Variables
+console.log(age);
+var age = 23;
+console.log(age);
+
+function foo(){
+    var age = 50; 
+    // variable object
+    console.log(age);
 }
 
-
-// b. Function Expression
-// the function can only be called after declaration 
-var retire = function(year){
-    console.log(60 - (2020 - year));
-}
-retire(1990);
+foo();
+// global object
+console.log(age);
 
 
-// Variables
+
+/* **********
+ *  Scoping
+ ************/
