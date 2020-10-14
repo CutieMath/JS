655c60861197d1fb45396ab4f689efe9b0b80978
CutieMath <cutie.math@protonmail.com>
2020-10-14 21:58:20 +1100

    finish Pig Game

--- 3-DOM/app.js
+++ 3-DOM/app.js
@@ -16,43 +16,86 @@ GAME RULES:
 ********************************************************************/
 
 // start from the most important variables
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer, dice, gamePlaying;
 
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
-// starting values before user interaction
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
 
-// Roll button function
+// Roll button
 document.querySelector('.btn-roll').addEventListener('click', function(){
+    if(gamePlaying) {
+        diceValue = Math.floor(Math.random() * 6) + 1;
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display = 'block';
+        diceDOM.src = 'dice-' + diceValue + '.png';
 
-    diceValue = Math.floor(Math.random() * 6) + 1;
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display = 'block';
-    diceDOM.src = 'dice-' + diceValue + '.png';
-
-    // update the round score IF the diceValue was not 1
-    if(diceValue !== 1) {
-        roundScore += diceValue;
-        document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    } else {
-        // change to next player and reset global value
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        document.getElementById('current-0').textContent = '0';
-        document.getElementById('current-1').textContent = '0';
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-        document.querySelector('.dice').style.display = 'none';
+        // update the round score if the diceValue was not 1
+        if(diceValue !== 1) {
+            roundScore += diceValue;
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
     }
 });
 
-document.querySelector('.btn-hold').addEventListener('click', function(){
 
+// Hold button
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    if(gamePlaying){
+        // make a global array to store scores for each player
+        scores[activePlayer] += roundScore;
+        // update UI
+        document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+        
+        if(scores[activePlayer] >= 20){
+            document.getElementById('name-' + activePlayer).innerHTML = "WON(｡◕‿◕｡) "
+            document.querySelector('.dice').style.display = 'none';
+            // apply css winner class
+            document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('winner');
+            document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+            // Use state variable to disable hold and roll button
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        }
+    }
 });
+
+
+// New game button
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+// repeated functions
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    // update UI
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+}
+
+function init(){
+    scores = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    gamePlaying = true; // state variable 
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('score-0').textContent = '0';
+    document.getElementById('score-1').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    // update Player text to all players, to replace Winner text
+    document.getElementById('name-0').textContent = 'Player 1';
+    document.getElementById('name-1').textContent = 'Player 2';
+    // update winner class from all players
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    // add active class to one player
+    document.querySelector('.player-0-panel').classList.add('active');
+}
