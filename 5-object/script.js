7852ffd6f942712322515bd1e2c84d5ed2c702d1
CutieMath <cutie.math@protonmail.com>
2020-10-18 19:46:54 +1100

    functions within function

--- 5-object/script.js
+++ 5-object/script.js
@@ -59,44 +59,87 @@
 // })
 
 
+// // *************************
+// // Objects and Primitives
+// // ************************************
+
+// // In primitives
+// var a = 23;
+// var b = a; // copy of reference not changed
+// a = 46;    // copy of value changed
+// console.log(a, b);
+
+
+// // However in objects
+// var obj1 = {
+//     name: 'baby', 
+//     age: 29
+// };
+// var obj2 = obj1;   // copy of reference
+// obj1.age = 30;     
+// console.log(obj2); // copy of reference changed
+// console.log(obj1); 
+
+// // Functions
+// var age = 29; 
+// var obj = {
+//     name: 'Baby', 
+//     city: 'Melbourne'
+// };
+
+// function change(a, b){
+//     a = 30; // copy of a new object
+//     b.city = 'London'; // copy of the reference 
+// }
+
+// change(age, obj);  
+// console.log(age);  // object not changed
+// console.log(obj.city); // reference changed
+
+// // Summary
+// // when use a = object: a copy of reference is created, no new object
+// // when use a = primitive: a copy of new object is created
+
+
 // *************************
-// Objects and Primitives
+// Functions within functions 
 // ************************************
 
-// In primitives
-var a = 23;
-var b = a; // copy of reference not changed
-a = 46;    // copy of value changed
-console.log(a, b);
+var years = [1990, 1965, 1966, 2005, 1999, 2015];
 
-
-// However in objects
-var obj1 = {
-    name: 'baby', 
-    age: 29
-};
-var obj2 = obj1;   // copy of reference
-obj1.age = 30;     
-console.log(obj2); // copy of reference changed
-console.log(obj1); 
-
-// Functions
-var age = 29; 
-var obj = {
-    name: 'Baby', 
-    city: 'Melbourne'
-};
-
-function change(a, b){
-    a = 30; // copy of a new object
-    b.city = 'London'; // copy of the reference 
+function arrayCalc(array, fn) {
+    var resultArr = [];
+    for(var i = 0; i < array.length; i ++){
+        resultArr.push(fn(array[i]));
+    }
+    return resultArr;
 }
 
-change(age, obj);  
-console.log(age);  // object not changed
-console.log(obj.city); // reference changed
+function calcAge(element){
+    return new Date().getFullYear() - element;
+}
 
-// Summary
-// when use a = object: a copy of reference is created, no new object
-// when use a = primitive: a copy of new object is created
+function isFullAge(element){
+    return element >= 18;
+}
 
+// only applicable for people between 18 to 81
+function maxHeartRate(element){
+    if(element < 18 || element > 81){
+        return -1;
+    }else {
+        return Math.round(206.9 - (0.67 * element));
+    }
+}
+
+console.log("First Function_________");
+var resultArr = arrayCalc(years, calcAge);
+console.log(resultArr);
+
+console.log("Second Function_________");
+var fullAgeArr = arrayCalc(resultArr, isFullAge);
+console.log(fullAgeArr);
+
+console.log("Third Function_________");
+var maxHeartRate = arrayCalc(resultArr, maxHeartRate);
+console.log(maxHeartRate);
