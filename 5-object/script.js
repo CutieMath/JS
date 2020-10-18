17f722a28332a803fc2e5057fee4fe01c11b2f87
CutieMath <cutie.math@protonmail.com>
2020-10-18 17:46:26 +1100

    primitives and objects

--- 5-object/script.js
+++ 5-object/script.js
@@ -38,24 +38,62 @@
 // major.printOccupation();
 
 
+// // *************************
+// // Object.create
+// // ************************************
+// var personProto = {
+//     calcAge: function(){
+//         console.log(new Date().getFullYear() - this.yearOfBirth);
+//     }
+// };
+
+// var baby = Object.create(personProto);
+// baby.name = 'Baby';
+// baby.yearOfBirth = '1998';
+// baby.occupation = 'Secret';
+
+// var cutie = Object.create(personProto, {
+//     name: { value: 'Cutie'},
+//     yearOfBirth: { value: 1999 }, 
+//     occupation: { value: 'Agent'}
+// })
+
+
 // *************************
-// Object.create
+// Objects and Primitives
 // ************************************
-var personProto = {
-    calcAge: function(){
-        console.log(new Date().getFullYear() - this.yearOfBirth);
-    }
+
+// In primitives
+var a = 23;
+var b = a; // copy of reference not changed
+a = 46;    // copy of value changed
+console.log(a, b);
+
+
+// However in objects
+var obj1 = {
+    name: 'baby', 
+    age: 29
+};
+var obj2 = obj1;   // copy of reference
+obj1.age = 30;     // copy of value
+console.log(obj2); // copy of reference not changed
+console.log(obj1); // copy of value changed
+
+// Functions
+var age = 29; 
+var obj = {
+    name: 'Baby', 
+    city: 'Melbourne'
 };
 
-var baby = Object.create(personProto);
-baby.name = 'Baby';
-baby.yearOfBirth = '1998';
-baby.occupation = 'Secret';
+function change(a, b){
+    a = 30; // not reflect outside the function
+    b.city = 'London'; // reflect outside the function
+}
 
-var cutie = Object.create(personProto, {
-    name: { value: 'Cutie'},
-    yearOfBirth: { value: 1999 }, 
-    occupation: { value: 'Agent'}
-})
+change(age, obj);  
+console.log(age);  
+console.log(obj.city); 
 
 
