6f3ec15782deb73381aae025a70cec6aace02393
CutieMath <cutie.math@protonmail.com>
2020-10-22 21:21:36 +1100

    define the function after the add button or Enter key is pushed

--- 6-moneyApp/app.js
+++ 6-moneyApp/app.js
@@ -31,6 +31,34 @@ var UIController = (function(){
 // *****************
 var controller = (function(moneyCtrl, UICtrl){
 
+
+    // 1. Add event listener for the button
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        // a. get input data
+        var type = document.querySelector('.add__type').value;
+        var description = document.querySelector('.add__description').value;
+        var moneyAmount = document.querySelector('.add__value').value;
+
+
+        // b. add to the data structure in moneyController
+
+        // c. update UI by adding to UIController
+
+        // d. Calculate money in the moneyController
+        
+        // e. update globale UI
+    });
+
+    // 2. Add event listener for Enter key!
+    document.addEventListener('keypress', function(event) {
+        if ( event.keyCode === 13 || event.which === 13 || event.key === 13) {
+            
+        }
+    });
+
+
+
+
 })(moneyController, UIController);
 
 
