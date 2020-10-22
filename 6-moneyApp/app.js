6c50e84108b0818dce4a485e6ea4ba263e65dad5
CutieMath <cutie.math@protonmail.com>
2020-10-22 20:53:24 +1100

    start from define modules

---
+++ 6-moneyApp/app.js
@@ -0,0 +1,36 @@
+// **********************
+// This project focus on: 
+// 1. Use module pattern
+// 2. Implement data encapsulation 
+// *******************************
+
+
+
+// *****************
+// Money Data Module
+// *****************
+var moneyController = (function(){
+    
+    
+})();
+
+
+
+// *****************
+// UI Module
+// *****************
+var UIController = (function(){
+
+})();
+
+
+
+
+// *****************
+// Globle Module 
+// *****************
+var controller = (function(moneyCtrl, UICtrl){
+
+})(moneyController, UIController);
+
+
